6d2718e7a082e56271cdf232db6fd7da5782be8f
Lucas Rodriguez <lucas.rodriguez9616@gmail.com>
2023-06-09 20:02:25 -0500

    Fix registry login command

--- apply.sh
+++ apply.sh
@@ -42,10 +42,10 @@ done
 
 aws s3 cp s3://"$s3_bucket"/kubeconfig.yaml .
 
-zarf tools kubectl get nodes -o wide --kubeconfig ./kubeconfig.yaml
-
 export KUBECONFIG="./kubeconfig.yaml"
+
+zarf tools kubectl get nodes -o wide --kubeconfig ./kubeconfig.yaml
 
 zarf init -a amd64 --components=git-server --confirm
 
-zarf package deploy zarf-package-* --confirm
+zarf package deploy oci://ghcr.io/lucasrod16/dubbd-k3d:0.2.1-amd64 --confirm
